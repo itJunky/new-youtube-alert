d4f1de3a65a66e92a6e20c0f5f2752dee5527b73
Teeny <largo@pop-os.localdomain>
2021-11-10 07:08:26 +0300

    Sending to TG

--- common.py
+++ common.py
@@ -1,7 +1,10 @@
 from db import engine, Video
+import telebot
+from conf import tgtoken, chat_id
 from sqlalchemy import and_
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+bot = telebot.TeleBot(tgtoken)
 session = scoped_session(sessionmaker(bind=engine))
 
 def in_db(vid):
@@ -17,4 +20,9 @@ def store_in_db(vid):
     session.add(new_video)
     session.commit()
 
+
+def send_to_tg(vid):
+    url = 'https://www.youtube.com/watch?v={}'.format(vid)
+    bot.send_message(chat_id, "Гринч сварганил новый видос \
+            {}".format(url))
 
--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 from googleapiclient.discovery import build
 import json
 from conf import token
-from common import in_db, store_in_db
+from common import in_db, store_in_db, send_to_tg
 
 with build('youtube', 'v3', developerKey=token) as youtube:
     response = youtube.search().list(
             channelId='UCfhQ8TdwGVP3YwCR6wQ2vqA',
-            maxResults=10,
+            maxResults=3,
             order='date',
             part='id, snippet'
     ).execute()
@@ -20,5 +20,6 @@ with build('youtube', 'v3', developerKey=token) as youtube:
         if not in_db(vid):
             #TODO send to TG
             print("{} - {}".format(vid, title))
+            send_to_tg(vid)
             store_in_db(vid)
 
